52f1202ffde4b68ee1d942670c74b442e4114497
yyeojung <hongshii0i0i@gmail.com>
2025-08-21 22:55:02 +0900

    AppRouter 상세, 커뮤니티, MyPage 빈화면 연결

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,11 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../page/home/Home";
 import PATHS from "./paths";
 import Sample from "../components/shared/Sample";
+import Detail from "@/page/detail/Detail";
+import Community from "@/page/community/Community";
+import MyPage from "@/page/myPage/MyPage";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path={PATHS.DETAIL.path} element={<Detail />} />
+      <Route path={PATHS.COMMUNITY.path} element={<Community />} />
+      <Route path={PATHS.MYPAGE.path} element={<MyPage />} />
       <Route path={PATHS.COMPONENTS.path} element={<Sample />} />
     </Routes>
   );
