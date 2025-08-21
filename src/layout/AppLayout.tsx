ea153dd6cca00e28ee505a9ebbed446058dc2b2b
yyeojung <hongshii0i0i@gmail.com>
2025-08-21 22:51:43 +0900

    홈화면 제외 헤더 추가

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,9 +1,19 @@
 import type { ReactNode } from "react";
 import NavigationBar from "./components/NavigationBar";
+import { useLocation } from "react-router-dom";
+import Header from "./components/Header";
+import PATHS from "@/routes/paths";
 
 const AppLayout = ({ children }: { children: ReactNode }) => {
+  const location = useLocation();
+
+  const isHome = location.pathname === "/";
+  const currentPathInfo = Object.values(PATHS).find((item) => item.path === location.pathname);
+
+  const headerTitle = currentPathInfo?.title;
   return (
     <div className="mx-auto min-h-screen max-w-md bg-gradient-to-br from-blue-50 to-indigo-100 pb-20">
+      {!isHome && <Header title={headerTitle} />}
       {children}
       <NavigationBar />
     </div>
---
+++ src/layout/components/Header.tsx
@@ -0,0 +1,30 @@
+import { ArrowLeft } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
+const Header = ({ title }: { title?: string }) => {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+  return (
+    <div className="border-b border-white/20 bg-white/90 px-6 py-4 backdrop-blur-sm">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <button
+            className="mr-3 flex h-8 w-8 cursor-pointer items-center justify-center rounded-full hover:bg-gray-100"
+            onClick={handleBack}
+          >
+            <ArrowLeft color="#4b5563" width={18} height={18} />
+          </button>
+          <h1 className="text-lg font-bold text-gray-800">{title}</h1>
+        </div>
+        <div className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full hover:bg-gray-100">
+          <i className="ri-settings-3-line text-lg text-gray-600"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
